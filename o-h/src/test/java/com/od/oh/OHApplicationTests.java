337924172222801e8c1f66bcfe7f084c0ae1b4b3
facheng.wang <facheng.wang@otms.com>
2017-09-11 16:47:56 +0800

    add config of log4j2.

--- o-h/src/test/java/com/od/oh/OHApplicationTests.java
+++ o-h/src/test/java/com/od/oh/OHApplicationTests.java
@@ -6,12 +6,16 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.od.oh.model.domain.OldDriver;
 
 @SpringBootTest
 public class OHApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(OHApplicationTests.class);
 
     @Test
     public void contextLoads() {
@@ -29,22 +33,26 @@ public class OHApplicationTests {
 
         OldDriver od = new OldDriver();
         session.beginTransaction();
-        // session.save(od);
-        // session.delete(od);
-        // session.update(od);
-
-        session.load(OldDriver.class, 1L);
-
-        System.out.println("=======================第一次查询======================= session --> " + session.hashCode());
-        System.out
-                .println("=======================End======================= --> " + session.find(OldDriver.class, 1L));
+//
+//
+//
+//
+//        // session.save(od);
+//        // session.delete(od);
+//        // session.update(od);
+//
+////        session.load(OldDriver.class, 1L);
+//
+        logger.info("=======================第一次查询======================= session --> " + session.hashCode());
+        logger.info("=======================End======================= --> " + session.find(OldDriver.class, 1L));
         session.getTransaction().commit();
         // session.update(od);
 
         Session session2 = sessionFactory.openSession();
-        System.out.println("=======================第二次查询======================= session --> " + session.hashCode());
-        System.out.println(
-                "=======================End======================= ---> " + session2.find(OldDriver.class, 1L));
+        logger.info("=======================第二次查询======================= session --> " + session.hashCode());
+        logger.info("=======================End======================= ---> " + session2.find(OldDriver.class, 1L));
+
+        logger.info("hello world");
 
     }
 
